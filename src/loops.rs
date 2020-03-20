808f7de7b1d6baf89e4fe8fee97f4fd09fcc01b7
TatriX <tatrics@gmail.com>
2020-03-20 20:49:43 +0100

    feat: Add HueLoop

--- src/loops.rs
+++ src/loops.rs
@@ -1,4 +1,7 @@
-use lighthouse::{colors::*, state, HueBridge};
+use ansi_term::Color;
+use lighthouse::{state, HueBridge};
+use palette::{Component, Hsl, Srgb, Yxy};
+use std::f64::consts::PI;
 use std::thread::sleep;
 use std::time::Duration;
 
@@ -15,27 +18,99 @@ impl Loop for TestLoop {
         "test"
     }
 
-    fn play(&self, h: &HueBridge) {
-        loop {
-            let steps = 30;
-            let dur = 30;
-            for i in 0..steps {
-                // 0 .. 1
-                // 0 .. 9
-                let color =
-                    palette::Hsl::new(f64::from(i * steps) / 2. * std::f64::consts::PI, 0.5, 0.5);
-                let (r, g, b) = palette::Srgb::from(color).into_components();
-                println!("{} {} {}", r, g, b);
-                let (r, g, b) = ((r * 255.) as u8, (g * 255.) as u8, (b * 255.) as u8);
-                println!("Setting color to {:?} from {:?}", (r, g, b), color);
-                h.state_by_ids(
-                    &[2, 3, 4],
-                    state!(on: true, bri: 125, xy: rgb_to_xy(r, g, b), transitiontime: dur),
-                )
-                .unwrap();
+    fn play(&self, _: &HueBridge) {
+        todo!()
+    }
+}
 
+pub struct SoloHueLoop;
+
+impl Loop for SoloHueLoop {
+    fn name(&self) -> &'static str {
+        "solo-hue"
+    }
+
+    fn play(&self, bridge: &HueBridge) {
+        loop {
+            let dur = 5;
+            let shift = 75.;
+            let lights: &[u8] = &[2, 3, 4];
+            for step in Steps::new(24) {
+                for (index, light) in lights.iter().enumerate() {
+                    let hue: Hsl<_, f64> =
+                        Hsl::new(step.to_degrees() + shift * index as f64, 0.8, 0.5);
+
+                    let (h, s, l) = hue.into_components();
+                    let rgb: Srgb<u8> = Srgb::from(hue).into_format();
+                    let (r, g, b) = rgb.into_components();
+
+                    println!(
+                        "{}",
+                        Color::RGB(r, g, b).paint(format!(
+                            "██████ {:3} {:.1} {:.1}",
+                            h.to_degrees().round(),
+                            s,
+                            l
+                        ))
+                    );
+
+                    let yxy = Yxy::from(hue);
+
+                    bridge
+                        .state_by_ids(
+                            &[*light],
+                            state!(
+                                on: true,
+                                bri: yxy.luma.convert(),
+                                xy: [yxy.x.convert(), yxy.y.convert()],
+                                transitiontime: dur
+                            ),
+                        )
+                        .unwrap();
+                }
+
+                println!();
                 sleep(Duration::from_millis(dur as u64 * 1000));
             }
         }
+    }
+}
+
+pub struct Steps {
+    num: i32,
+    index: i32,
+}
+
+impl Steps {
+    pub fn new(num: i32) -> Self {
+        Self { num, index: 0 }
+    }
+}
+
+impl Iterator for Steps {
+    type Item = f64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index < self.num {
+            let index = self.index as f64;
+            self.index += 1;
+            Some(index * 2. * PI / f64::from(self.num))
+        } else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_steps() {
+        let expected = (0..360).step_by(15).collect::<Vec<_>>();
+        let got = Steps::new(24)
+            .map(|step| step.to_degrees().round() as usize)
+            .collect::<Vec<_>>();
+        assert_eq!(got, expected);
     }
 }
